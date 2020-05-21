1c0fb2a303d474b4ab67af341b5d4e6afdb15e26
YoungRok <51745277+robin9804@users.noreply.github.com>
2020-05-21 16:30:57 +0900

    webcroller init

---
+++ croll.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 조선일보 url
+url1 = 'https://www.chosun.com/'
+
+# 특정 사이트에 접속하는 요청 객체 생성,
+r1 = requests.get(url1)
+
+# BeatifulSoup 객체 생성
+soup = BeautifulSoup(r1.content, "html.parser", from_encoding='utf-8')
+
+print(soup)
+
+h2 = soup.select('h2')
+
+class Crolling:
+    def __init__(self, url):
+        self.url = url
+        self.request = requests.get(self.url)
+        self.soup = BeautifulSoup(self.request.content, "html.parser", from_encoding='utf-8') # BeatifulSoup 객체
+
+    def get_title(self):
+        k = self.soup.select('a')
+        title = []
+        for i in range(len(k)):
+            title.append(k[i])
+        return title
+
+
